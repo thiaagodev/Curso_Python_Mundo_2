a6d2ac59e5d65eb5434dace45958cf8ab377fb05
Thiago Ferreira <thiaago.dev@gmail.com>
2021-01-11 18:49:35 -0300

    Aula 012

---
+++ pythonteste/aula012a.py
@@ -0,0 +1,9 @@
+nome = input('Qual é o seu nome? ')
+
+if nome == 'Thiago':
+    print('Que nome bonito!')
+elif nome != 'Thiago':
+    print('Seu nome é bem normal')
+    
+print('Tenha um bom dia, {}!'.format(nome))
+
